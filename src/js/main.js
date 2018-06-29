3798271bcd3f1fcfab22324fcb09c2c5f871b06f
Nemanja <nemanja_muncan@yahoo.com>
2018-06-29 21:44:00 +0200

    added complete AboutPage and slickScroll lib

--- src/js/main.js
+++ src/js/main.js
@@ -48,3 +48,11 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 })
+
+
+//slickSlider
+$('.slider').slick({
+  infinite:true,
+  slideToShow:1,
+  slideToScroll:1
+});
